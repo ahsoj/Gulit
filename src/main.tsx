0338884768ffa7ec86586ea586365fa794a4c266
eyasu chaka <daa282386@gmail.com>
2024-03-31 18:53:34 +0300

    new update

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import eruda from "eruda";
 
 import WebApp from "@twa-dev/sdk";
 
-WebApp.ready();
+const availablePlatform = ["android", "android_x", "ios"]
+
+WebApp.isVersionAtLeast('6.1') && WebApp.ready();
 WebApp.enableClosingConfirmation();
-WebApp.setHeaderColor("#0094FF")
+WebApp.setHeaderColor("#0094FF");
+
+eruda.init()
+
+if (availablePlatform.map((platform) => WebApp.platform === platform)) {
+  console.log("Luckily You have access to this app")
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
